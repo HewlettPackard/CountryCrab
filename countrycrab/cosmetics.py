4fb9a3d83325647e8b1840f9c95c906680eaef96
Giacomo Pedretti <giacomo.pedretti@hpe.com>
2024-09-03 10:50:27 -0700

    Added function to keep fig axis at constant size

--- countrycrab/cosmetics.py
+++ countrycrab/cosmetics.py
@@ -1,19 +1,3 @@
-###
-# Copyright (2024) Hewlett Packard Enterprise Development LP
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-###
-
 import matplotlib.pyplot as plt
 ###
 # Copyright (2024) Hewlett Packard Enterprise Development LP
@@ -31,7 +15,7 @@ import matplotlib.pyplot as plt
 # limitations under the License.
 ###
 
-
+from mpl_toolkits.axes_grid1 import Divider, Size
 
 params = {'legend.fontsize': 'x-large',
           'figure.figsize': (4, 4),
@@ -53,3 +37,18 @@ def set_size(w,h, ax=None):
     figw = float(w)/(r-l)
     figh = float(h)/(t-b)
     ax.figure.set_size_inches(figw, figh)
+
+
+def new_fig(hs=3,vs=3):
+    fig = plt.figure(figsize=(6, 6))
+    
+    # The first items are for padding and the second items are for the Axes.
+    # sizes are in inch.
+    h = [Size.Fixed(1.0), Size.Fixed(hs)]
+    v = [Size.Fixed(0.7), Size.Fixed(vs)]
+    
+    divider = Divider(fig, (0, 0, 1, 1), h, v, aspect=False)
+    ax = fig.add_axes(divider.get_position(),
+                      axes_locator=divider.new_locator(nx=1, ny=1))
+
+    return ax
